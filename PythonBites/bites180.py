b7f96affdecd9fdf93980f517dd86434b4af8e11
melkorounet <melkorounet@gmail.com>
2019-10-16 16:56:42 +0200

    new chapter

---
+++ PythonBites/bites180.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    print(type(countries))
+    data_list = []
+    formatted_data = data.split('\n')
+    for entry in formatted_data :
+        data_list.append(entry.split(','))
+
+    for name, surname, country in data_list:
+        #print(data_list)
+        if len(country) == 2:
+            #print(type(countries))
+            countries[country].append(surname + " " + name)
+    return countries        
+
+group_names_by_country(data)
