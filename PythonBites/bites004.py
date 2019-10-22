09f33c801d001d665d93885b9011def568e87c63
melkorounet <melkorounet@gmail.com>
2019-10-22 16:00:11 +0200

    new bite

---
+++ PythonBites/bites004.py
@@ -0,0 +1,30 @@
+import os
+from collections import Counter
+import urllib.request
+
+# prep
+tempfile = os.path.join('.', 'feed')
+urllib.request.urlretrieve('http://bit.ly/2zD8d8b', tempfile)
+
+with open(tempfile) as f:
+    content = f.read().lower()
+
+
+# start coding
+
+def get_pybites_top_tags(n=10):
+    """use Counter to get the top 10 PyBites tags from the feedget_pybites_top_tags
+       data already loaded into the content variable"""
+
+    tag_list=[]
+    final_list=[]
+    tag_occur = Counter()
+    lines=content.split("<category>")
+    for texte in lines : 
+        if "</category>" in texte : 
+            tag_list.append(texte.split("</category>"))
+    for element in tag_list :
+        final_list.append(element[0])
+    print(Counter(final_list).most_common(n))
+
+get_pybites_top_tags()
---
+++ PythonBites/bites006.py
@@ -0,0 +1,56 @@
+"""Checks community branch dir structure to see who submitted most
+   and what challenge is more popular by number of PRs"""
+from collections import Counter, namedtuple
+import os
+import urllib.request
+
+# prep
+
+tempfile = os.path.join('.', 'dirnames')
+urllib.request.urlretrieve('http://bit.ly/2ABUTjv', tempfile)
+
+IGNORE = 'static templates data pybites bbelderbos hobojoe1848'.split()
+
+users, popular_challenges = Counter(), Counter()
+
+Stats = namedtuple('Stats', 'user challenge')
+
+with open(tempfile) as f:
+    content = f.read().lower()
+
+# code
+
+def gen_files():
+    """Return a generator of dir names reading in tempfile
+
+       tempfile has this format:
+
+       challenge<int>/file_or_dir<str>,is_dir<bool>
+       03/rss.xml,False
+       03/tags.html,False
+       ...
+       03/mridubhatnagar,True
+       03/aleksandarknezevic,True
+
+       -> use last column to filter out directories (= True)
+    """
+    dir_names=[]
+    liste=content.split("\n")
+    for line in liste : 
+        if "true" in line:
+            dir_names.append(line)
+    print(dir_names)
+    pass
+
+
+
+def diehard_pybites():
+    """Return a Stats namedtuple (defined above) that contains the user that
+       made the most PRs (ignoring the users in IGNORE) and a challenge tuple
+       of most popular challenge and the amount of PRs for that challenge.
+       Calling this function on the dataset (held tempfile) should return:
+       Stats(user='clamytoe', challenge=('01', 7))
+    """
+    pass
+
+gen_files()
